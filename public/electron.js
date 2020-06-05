8eebaacc75b9671541557a9a7185e422a205ef30
Greif Matthias <greif.matthias@outlook.com>
2020-06-05 23:16:32 +0200

    Reworked ElectronBase

--- public/electron.js
+++ public/electron.js
@@ -6,49 +6,61 @@ const path = require('path');
 
 const isMac = process.platform === 'darwin';
 let isQuiting;
-let application;
 
 
 
-const init = () => {
+class Application {
 
 
-    // Create window
-    application = new BrowserWindow({
-        width: 1200,
-        height: 800,
-        backgroundColor: '#FFFFFF'
-    });
+    window;
 
 
-    // Loadup app
-    const _URL = process.env.ELECTRON_START_URL || url.format({
-        pathname: path.join(__dirname, '/../build/index.html'),
-        protocol: 'file:',
-        slashes: true
-    });
-    application.loadURL(_URL);
-
-
-    // Subscribe when window is closing
-    application.on('close', (event) => {
-        if (isMac && !isQuiting) {
-            event.preventDefault();
-            application.minimize();
-
-            return false;
-        }
-    });
-};
+    constructor() { }
 
 
 
-// Check if app request to quit
-app.on('before-quit', function () {
-    isQuiting = true;
-});
+    init = () => {
 
+        // Create window
+        this.window = new BrowserWindow({
+            width: 1200,
+            height: 800,
+            backgroundColor: '#FFFFFF'
+        });
+
+
+        // Loadup app
+        const _URL = process.env.ELECTRON_START_URL || url.format({
+            pathname: path.join(__dirname, '/../build/index.html'),
+            protocol: 'file:',
+            slashes: true
+        });
+
+        this.window.loadURL(_URL);
+
+
+        // Subscribe when window is closing
+        this.window.on('close', (event) => {
+            if (isMac && !isQuiting) {
+                event.preventDefault();
+                this.window.minimize();
+
+                return false;
+            }
+        });
+    };
+}
+
+
+
+// Check if app quit is requested
+app.on('before-quit', () => isQuiting = true);
+
+
+
+// Init Application
+const application = new Application();
 
 
 // Startup when Electron is initialised
-app.whenReady().then(init);
+app.whenReady().then(application.init);
